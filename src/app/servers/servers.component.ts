74d52dd618ab3795391b6d21ef21ee268513a828
Samrat Kafle <skafle3@leomail.tamuc.edu>
2023-03-13 20:34:30 -0500

    Created few mthods and added bootstrap form.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  ngOnInit(){
+  }
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created.'
+  }
+  onUpdateServerName(event: any){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 
 }
